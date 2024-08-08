c69a15d652db4d671882f5bee5cf234adf5b7d07
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-08 17:09:06 +0200

    feat: understand static memory allocation

--- pointers.c
+++ pointers.c
@@ -9,11 +9,13 @@ struct employee_t {
   bool ismanager;
 };
 
-void initialize_employee(struct employee_t *e) {
-  e->id = 0;
+int initialize_employee(struct employee_t *e) {
+  static int numEmployees = 0;
+  numEmployees++;
+  e->id = numEmployees;
   e-> income = 0;
   e->ismanager = false;
-  return;
+  return numEmployees;
 };
 
 int main() {
@@ -29,6 +31,12 @@ int main() {
     printf("The allocator failed\n");
     return -1;
   };
+
+  for (int i= 0; i<n; i++) {
+    int id = initialize_employee(&employees[i]);
+    printf("New Employee, ID is %d\n", id);
+  };
+
   initialize_employee(&employees[0]);
 
   printf("%f\n", employees[0].income);
