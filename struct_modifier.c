7548dd3014ebac6705835457ce82d959cc1bc5ce
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-08 15:04:31 +0200

    feat: understand struct modifier

---
+++ struct_modifier.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_EMPLOYEES 1000
+
+__attribute__((__packed__)) struct employee_t {
+  int id;
+  char firstname[64];
+  char lastname[64];
+  float income;
+  bool ismanager;
+};
+
+int main() {
+  struct employee_t employees[MAX_EMPLOYEES];
+  printf("Size of employee: %d\n", sizeof(struct employee_t));
+};
