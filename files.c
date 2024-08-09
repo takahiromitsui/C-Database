62bad3eca2d9d4c0f125a372211f2bc1d089002d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-09 08:35:50 +0200

    feat: file Output

--- files.c
+++ files.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int main() {
-  int fd = open("./abb", O_RDONLY); 
+int main(int argc, char *argv[]) {
+  if(argc !=2) {
+    printf("Usage: %s <filename>\n", argv[0]);
+    return 0;
+  }
+
+
+  int fd = open("./myfile", O_RDWR|O_CREAT, 0644); 
   if (fd==-1) {
     perror("open");
     return -1;
-  }
+  }q\
 }
