32b1575b5281c1ca62598ad6980ce8b05d4cea66
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-09 09:46:57 +0200

    feat: read file metadata

--- files.c
+++ files.c
@@ -2,22 +2,56 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/stat.h>
+
+struct database_header_t {
+  unsigned short version;
+  unsigned short employees;
+  unsigned int filesize;
+};
+
 
 int main(int argc, char *argv[]) {
+  struct database_header_t head= {0};
+  struct stat dbStat = {0};
+
   if(argc !=2) {
     printf("Usage: %s <filename>\n", argv[0]);
     return 0;
   }
 
 
-  int fd = open("./myfile", O_RDWR|O_CREAT, 0644); 
+  int fd = open(argv[0], O_RDWR|O_CREAT, 0644); 
   if (fd==-1) {
     perror("open");
     return -1;
   }
 
-  char *mydata = "hello there file!\n";
-  write(fd, mydata, strlen(mydata));
+  if(read(fd, &head, sizeof(head)) != sizeof(head)) {
+    perror("read");
+    close(fd);
+    return -1;
+  }
+
+  printf("DB_Version: %u\n", head.version);
+  printf("DB Number of Employees: %u\n", head.employees);
+  printf("DB File Length: %u\n", head.filesize);
+
+  if (fstat(fd, &dbStat) < 0) {
+    perror("stat");
+    close(fd);
+    return -1;
+  }
+  printf("DB File Length, reported by stat: %lld\n", dbStat.st_size);
+
+  if (dbStat.st_size != head.filesize) {
+    printf("GET OUTTA HERE HACKER!\n");
+    close(fd);
+    return -2;
+  };
+
   close(fd);
   return 0;
 }
