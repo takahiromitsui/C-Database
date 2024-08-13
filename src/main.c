2e8f1c326a8fca70785d2ff3938a1619b11bc4ab
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-13 11:11:14 +0200

    feat: add usage

--- src/main.c
+++ src/main.c
@@ -2,6 +2,13 @@
 #include <stdbool.h>
 #include <getopt.h>
 
+void print_usage(char *argv[]) {
+  printf("Usage: %s -n -f <database file>\n", argv[0]);
+  printf("\t -n - create new database file\n");
+  printf("\t -f - (required) path to database file\n");
+  return;
+};
+
 int main(int argc, char *argv[]) {
   char *filepath = NULL;
   bool newfile = false;
@@ -21,6 +28,13 @@ int main(int argc, char *argv[]) {
         return -1;
     }
   }
+
+  if (filepath == NULL) {
+    printf("Filepath is a required argument\n");
+    print_usage(argv);
+    return 0;
+  }
+
   printf("Newfile: %d\n", newfile);
   printf("Filepath: %s\n", filepath);
   return 0;
