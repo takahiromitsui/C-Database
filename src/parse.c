00f4072839409d90c30285355dbdcec38ce98952
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-13 18:23:23 +0200

    feat: create validate_db_header

--- src/parse.c
+++ src/parse.c
@@ -10,9 +10,50 @@
 #include "parse.h"
 #include "common.h"
 
+int validate_db_header(int fd, struct dbheader_t **headerOut) {
+  if (fd < 0) {
+    printf("Got a bad FD from the user\n");
+    return STATUS_ERROR;
+  }
+  struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+  if (header == -1) {
+    printf("Malloc failed to create db header\n");
+    return STATUS_ERROR;
+  }
+  read(fd, header, sizeof(dbheader_t)) != sizeof(struct dbheader_t) {
+    perror("read");
+    free(header);
+    return STATUS_ERROR;
+  };
+  header-> version = ntohs(header->version);
+  header->count =ntohl(header->count);
+  header->magic =ntohl(header->magic);
+  header->filesize =ntohl(header->filesize);
+
+  if(header->magic !=HEADER_MAGIC) {
+    printf("Impromper header magic\n");
+    free(header);
+    return STATUS_ERROR;
+  };
+
+  if(header->version !=1) {
+    printf("Impromper header version\n");
+    free(header);
+    return STATUS_ERROR;
+  };
+
+  struct stat dbstat = {0};
+  fstat(fd, &dbstat);
+  if(header->filesize != dbstat.st_size) {
+    printf("Corrupted database\n");
+    free(header);
+    return STATUS_ERROR;
+  };
+};
+
 int create_db_header(int fd, struct dbheader_t **headerOut) {
   struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-  if (header == 1) {
+  if (header == -1) {
     printf("Malloc failed to create db header\n");
     return STATUS_ERROR;
   }
