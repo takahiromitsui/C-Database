746602cd141d2ffaf6648b7d4dcbe4292635ddf6
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-08-13 12:05:25 +0200

    fix: fix conditional statement

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include <stdbool.h>
 #include <getopt.h>
 
+#include "common.h"
+#include "file.h"
+
 void print_usage(char *argv[]) {
   printf("Usage: %s -n -f <database file>\n", argv[0]);
   printf("\t -n - create new database file\n");
@@ -13,6 +16,8 @@ int main(int argc, char *argv[]) {
   char *filepath = NULL;
   bool newfile = false;
   int c;
+
+  int dbfd = -1;
 
   while((c = getopt(argc, argv, "nf:")) != -1) {
     switch (c) {
@@ -34,6 +39,20 @@ int main(int argc, char *argv[]) {
     print_usage(argv);
     return 0;
   }
+
+  if(newfile) {
+    dbfd = create_db_file(filepath);
+    if(dbfd == STATUS_ERROR) {
+      printf("Unable to create database file \n");
+      return -1;
+    } 
+  } else {
+      dbfd = open_db_file(filepath);
+      if(dbfd == STATUS_ERROR) {
+        printf("Unable to open database file \n");
+        return -1;
+      }
+    };
 
   printf("Newfile: %d\n", newfile);
   printf("Filepath: %s\n", filepath);
